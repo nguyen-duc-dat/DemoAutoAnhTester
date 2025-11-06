ad5d6eb3a922951daa21fe681e79b8e8d5fb0a35
Nguyễn Đức Đạt <nguyenduc01012k@gmail.com>
2025-11-06 21:49:30 +0700

    edit main.java

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        System.out.print("Hello and welcome!");
+        System.out.println("Hello and welcome!");
+        System.out.println("How are you");
     }
 }
